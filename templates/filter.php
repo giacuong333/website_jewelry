922d44bdd91968f62e8d0573149bea365a3ed8e7
Will <dinhvanvo510@gmail.com>
2024-05-10 21:38:56 +0700

    .

--- templates/filter.php
+++ templates/filter.php
@@ -21,6 +21,30 @@ if(isset($_POST['min_price']) && isset($_POST['max_price'])){
     $count = mysqli_num_rows($query);
     if($count == 0){
         echo '<h2>No Product Found</h2>';
+    }else{
+        echo '<div class="product-view"><div class="row">';
+        while ($row = mysqli_fetch_array($query)) {
+            echo '
+                <div class="product-item" data-productid="'.$row["id"].'">
+                    <div class="product">
+                        <div class="product-img">
+                            <a href="#">
+                                <img class="img-prd" src="'.$row['thumbnail'].'" alt="anh san pham ">
+                            </a>
+                            <div class="cart-icon">
+                                <i class="fa fa-shopping-cart"></i>
+                            </div>
+                        </div>
+                        <div class="product-name">
+                            <p class="big"><a href="productdetails.php?data-productid='.$row["id"].'">'.$row['title'].'</a></p>
+                        </div>
+                        <div class="product-price">
+                            '.$row['price'].'đ
+                        </div>
+                    </div>
+                </div>';
+        }
+        echo '</div></div>';
     }
     
 }
